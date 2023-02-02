0b93456793959f1c929e5d79e4a4d240188ed813
Lowstack <laurence.olivier.foisy@gmail.com>
2023-02-02 15:02:04 -0500

    Beau tit-graph

--- dataPractice.R
+++ dataPractice.R
@@ -1,22 +1,31 @@
 library(dplyr)
-library(ggplot2)
 library(wbstats)
+library(ggplot2)
+library(scales)
 
 ExportUSA <- wb_data(country = "USA", indicator ="NE.EXP.GNFS.CD")
 
 
-tail(ExportUSA, n=50)
-
 ExportUSA %>% 
   ggplot(aes(date, NE.EXP.GNFS.CD))+
-  geom_point()+
-  geom_line()+ 
-  labs(title = "What's up", x = "Années", y = "Exportations en $USD" )+
+  geom_point(colour="red")+
+  geom_line(colour="blue")+ 
+  labs(title = "Exportations amércaines en $USD de 1970 à 2020", x = "Années", y = "Exportations en $USD" )+
+  scale_y_continuous(expand = c(0,0),
+                     limits=c(0, 3000000000000), 
+                     labels = label_number(suffix = "T", scale = 1e-11),
+                     breaks=seq(from = 0, to = 3000000000000, by = 500000000000)
+                     )+
+  scale_x_continuous(limits = c(1970, 2022), 
+                     expand = c(0,0),
+                     breaks=seq(from = 1970, to = 2020, by = 5)
+                     )+
+  geom_smooth()+
+  theme_classic()+
   ggsave(
     filename = "bigGraphyBoii.pdf",
     scale = 1,
     device = "pdf",
     width = 8,
-    height = 8,
+    height = 8
     )
-
