5883643d693f3083b59aefe0a6b34cd3b3eb572f
L.-O <laurence.olivier.foisy@gmail.com>
2023-02-08 14:21:54 -0500

    Ajout du graphique Chine

--- dataPractice.R
+++ dataPractice.R
@@ -4,7 +4,8 @@ library(ggplot2)
 library(scales)
 
 ExportUSA <- wb_data(country = "USA", indicator ="NE.EXP.GNFS.CD")
-
+ExportChina <- wb_data(country = "CHN", indicator ="NE.EXP.GNFS.CD")
+ExportChina
 
 ExportUSA %>% 
   ggplot(aes(date, NE.EXP.GNFS.CD))+
@@ -29,3 +30,27 @@ ExportUSA %>%
     width = 8,
     height = 8
     )
+
+ExportChina %>% 
+  ggplot(aes(date, NE.EXP.GNFS.CD))+
+  geom_point(colour="red")+
+  geom_line(colour="blue")+ 
+  labs(title = "Exportations chinoises en $USD de 1970 à 2020", x = "Années", y = "Exportations en $USD" )+
+  scale_y_continuous(expand = c(0,0),
+                     limits=c(0, 3000000000000), 
+                     labels = label_number(suffix = "T", scale = 1e-11),
+                     breaks=seq(from = 0, to = 3000000000000, by = 500000000000)
+  )+
+  scale_x_continuous(limits = c(1970, 2022), 
+                     expand = c(0,0),
+                     breaks=seq(from = 1970, to = 2020, by = 5)
+  )+
+  geom_smooth()+
+  theme_classic()+
+  ggsave(
+    filename = "bigGraphyBoiiChina.pdf",
+    scale = 1,
+    device = "pdf",
+    width = 8,
+    height = 8
+  )
